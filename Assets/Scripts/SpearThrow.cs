babdcbfd8f9652e3074bc77cb355930e5fccf4d5
Levi <lw03232@gmail.com>
2021-02-16 09:28:46 -0600

    Minor code cleanup

--- Assets/Scripts/SpearThrow.cs
+++ Assets/Scripts/SpearThrow.cs
@@ -21,26 +21,32 @@ public class SpearThrow : MonoBehaviour
             pos.x += .75f;
             pos.y += .75f;
             GameObject shot = Instantiate(projectile, transform.position, transform.rotation);
-            Facing dir = gameObject.GetComponent<PlayerControl>().direction;
-            if (dir == Facing.Left)
-            {
-                shot.transform.Rotate(new Vector3(0,0,180));
-                shot.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
-            }
-            else if (dir == Facing.Up)
-            {
-                shot.transform.Rotate(new Vector3(0, 0, 90));
-                shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
-            }
-            else if (dir == Facing.Down)
-            {
-                shot.transform.Rotate(new Vector3(0, 0, 270));
-                shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10);
-            }
-            else
-            {
-                shot.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
-            }
+            SetShotVelocity(shot);
+            
+        }
+    }
+
+    private void SetShotVelocity(GameObject shot)
+    {
+        Facing dir = gameObject.GetComponent<PlayerControl>().direction;
+        if (dir == Facing.Left)
+        {
+            shot.transform.Rotate(new Vector3(0, 0, 180));
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+        }
+        else if (dir == Facing.Up)
+        {
+            shot.transform.Rotate(new Vector3(0, 0, 90));
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+        }
+        else if (dir == Facing.Down)
+        {
+            shot.transform.Rotate(new Vector3(0, 0, 270));
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10);
+        }
+        else
+        {
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
         }
     }
 }
