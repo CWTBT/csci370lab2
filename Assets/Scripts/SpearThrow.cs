f0f6a29bd9a090cab5e04ba40a12236312b3b982
Levi <lw03232@gmail.com>
2021-02-15 19:15:58 -0600

    On-click instantiation, spear art

---
+++ Assets/Scripts/SpearThrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearThrow : MonoBehaviour
+{
+    public GameObject projectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //https://gamebridgeu.wordpress.com/2017/02/12/instantiate/
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject shot = Instantiate(projectile, transform.position, transform.rotation);
+            shot.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0) * 10);
+        }
+    }
+}
