68032a8bfdd51ad340e31f07da23b45be8a11acb
Levi <lw03232@gmail.com>
2021-02-15 20:11:12 -0600

    Sprites now change with movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -12,10 +12,18 @@ public class PlayerControl : MonoBehaviour
 
     public float runSpeed = 10f;
     private float moveLimiter = 0.7f;
+
+    public Sprite right;
+    public Sprite left;
+    public Sprite up;
+    public Sprite down;
+    private SpriteRenderer spr;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spr = this.GetComponent<SpriteRenderer>();
+        if (spr.sprite == null) spr.sprite = right;
     }
 
     // Update is called once per frame
@@ -33,5 +41,26 @@ public class PlayerControl : MonoBehaviour
             vertical *= moveLimiter;
         }
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        setSprite();
+    }
+
+    private bool HorizontalMagnitudeGreater()
+    {
+        return Mathf.Abs(body.velocity.x) > Mathf.Abs(body.velocity.y);
+    }
+
+    private void setSprite()
+    {
+        if (body.velocity == Vector2.zero) return;
+        else if (HorizontalMagnitudeGreater())
+        {
+            if (body.velocity.x > 0) spr.sprite = right;
+            else spr.sprite = left;
+        }
+        else
+        {
+            if (body.velocity.y > 0) spr.sprite = up;
+            else spr.sprite = down;
+        }
     }
 }
--- Assets/Scripts/SpearThrow.cs
+++ Assets/Scripts/SpearThrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpearThrow : MonoBehaviour
 {
     public GameObject projectile;
+    public Sprite spr;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,8 @@ public class SpearThrow : MonoBehaviour
         //https://gamebridgeu.wordpress.com/2017/02/12/instantiate/
         if (Input.GetMouseButtonDown(0))
         {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(pos);
             GameObject shot = Instantiate(projectile, transform.position, transform.rotation);
             shot.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
         }
